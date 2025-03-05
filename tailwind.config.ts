9d99382c43b430c123ec68369fc3ab01b1d21ec4
ssouvikk <souvik.edu.mondal@gmail.com>
2025-03-05 20:11:40 +0530

    css issue fixingin

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+// tailwind.config.js
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: 'class',
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "var(--primary)",
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
